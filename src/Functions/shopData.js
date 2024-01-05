4a3f417577a73d6a3c485e0a1191a0c6f4c93335
Saahan <smombasa1989@gmail.com>
2024-01-05 08:48:54 +0530

    update supplement store data

---
+++ src/Functions/shopData.js
@@ -0,0 +1,28 @@
+let shopData = [
+  {
+    name: "Pre-Workout Creatin and Protein Monohydrate",
+    price: 800,
+    description: "A great pre-workout formula for the body",
+    weight: "0.22 lb",
+  },
+  {
+    name: "Vitals Collagen",
+    price: 769,
+    description: "Comes in a refreshing watermelon flavour",
+    weight: "200 g",
+  },
+  {
+    name: "Super Gainer Protein Powder",
+    price: 3599,
+    description: "Comes in chocolate flavour",
+    weight: "11 lb",
+  },
+  {
+    name: "High Protein Oats",
+    price: 1229,
+    description: "A great way to start the day",
+    weight: "2 kg",
+  },
+];
+
+export {shopData};
--- src/components/SupplementStore.js
+++ src/components/SupplementStore.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import "../styles/views.css";
+import { shopData } from "../Functions/shopData";
+import { Card, Col, Row } from "react-bootstrap";
 
 export default function SupplementStore() {
   return (
-    <div>SupplementStore</div>
-  )
+    <div className="container">
+      <h1>Supplement Store</h1> <hr />
+      <Row>
+        {shopData.map((item) => {
+          return (
+            <Col lg={4} sm={6} md={6}>
+              <Card style={{ marginBottom: "20px", height: "200px" }}>
+                <Card.Title style={{ height: "50px", padding: "5px" }}>
+                  {item.name}
+                </Card.Title>
+                <Card.Body>
+                  <span style={{ fontSize: "larger" }}>₹{item.price}</span>{" "}
+                  <br />
+                  <span>{item.weight}</span> <br />
+                  <span>{item.description}</span>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
+  );
 }
